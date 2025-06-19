a14709b298f71ae483303edb957d519d6953c062
Younes-KH97 <younes.khellaf97@gmail.com>
2025-06-20 01:05:55 +0200

    add invoice operations

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,24 +1,33 @@
-from odoo import models, fields, api
+from odoo import models
 from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def set_sold(self):
-        res = super().set_sold()  # call the original logic
+        res = super().set_sold()
 
-        for property in self:
-            if not property.buyer_id:
-                raise UserError("Cannot create invoice without a buyer.")
-            
-            invoice = self.env['account.move'].create({
-                'partner_id': property.buyer_id.id,
-                'move_type': 'out_invoice',
-                'invoice_line_ids': [(0, 0, {
-                    'name': f"Property sold: {property.name}",
+        if not self.buyer_id:
+            raise UserError("Cannot create invoice without a buyer.")
+
+        commission = self.selling_price * 0.06
+        admin_fee = 100.00
+
+        self.env['account.move'].create({
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice',
+            'invoice_line_ids': [
+                (0, 0, {
+                    'name': f"Commission - {self.name}",
                     'quantity': 1,
-                    'price_unit': property.selling_price,
-                })],
-            })
+                    'price_unit': commission,
+                }),
+                (0, 0, {
+                    'name': "Administrative Fees",
+                    'quantity': 1,
+                    'price_unit': admin_fee,
+                }),
+            ],
+        })
 
         return res
