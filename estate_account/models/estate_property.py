779378d0f5b7333f3d0e7e6a2ceb4bb713610ef5
Younes-KH97 <younes.khellaf97@gmail.com>
2025-09-03 15:54:51 +0200

    add demo data

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,5 +1,5 @@
 from odoo import models
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, AccessError
 
 class EstateProperty(models.Model):
     _inherit = 'estate.property'
@@ -13,7 +13,14 @@ class EstateProperty(models.Model):
         commission = self.selling_price * 0.06
         admin_fee = 100.00
 
-        self.env['account.move'].create({
+        try:
+            self.check_access_rights('write', raise_exception=True)
+            # self.check_access_rule('write', raise_exception=True)
+        except AccessError:
+            raise UserError("You don't have permission to write this property.")
+
+        # print(" reached ".center(100, '='))
+        self.env['account.move'].sudo().create({
             'partner_id': self.buyer_id.id,
             'move_type': 'out_invoice',
             'invoice_line_ids': [
